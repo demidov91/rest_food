0fa2c761d94f4acba4f924c44a6c288d12c5ccff
Dzmitry Dziamidau <dzmitry.dziamidau@datarobot.com>
2019-08-17 13:48:19 +0300

    Reuse existing translation.

--- createmessages.sh
+++ createmessages.sh
@@ -1,6 +1,6 @@
 xgettext -L Python --from-code=utf-8 ./rest_food/*/*.py -o rest_food/locale/_common.po
 
-msgmerge rest_food/locale/ru/messages.po rest_food/locale/_common.po -o rest_food/locale/ru/LC_MESSAGES/messages.po --lang=ru
-msgmerge rest_food/locale/be/messages.po rest_food/locale/_common.po -o rest_food/locale/be/LC_MESSAGES/messages.po --lang=be
+msgmerge rest_food/locale/ru/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/ru/LC_MESSAGES/messages.po --lang=ru.UTF-8
+msgmerge rest_food/locale/be/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/be/LC_MESSAGES/messages.po --lang=be.UTF-8
 
 rm rest_food/locale/_common.po
