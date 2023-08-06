67526047661be0b37d9cef11059727222f7d4dba
Dzmitry <demidov91@mail.ru>
2023-08-06 22:52:41 +0200

    Add a missing env-var

--- rest_food/settings.py
+++ rest_food/settings.py
@@ -10,10 +10,10 @@ logger = logging.getLogger(__name__)
 BASE_DIR = os.path.dirname(__file__)
 
 
-def env_var(var_name: str):
+def env_var(var_name: str, default=None):
     if var_name not in os.environ:
         logger.warning('%s env variable was not found.', var_name)
-        return None
+        return default
 
     return os.environ[var_name]
 
@@ -27,7 +27,7 @@ DB_NAME = env_var('DB_NAME')
 STAGE = env_var('STAGE')
 
 # Mon, Mon, Dz, G, I, Dz(add)
-TEST_TG_CHAT_ID = env_var('TEST_TG_CHAT_ID').split(',')
-TEST_TG_CHAT_ID.extend([int(x) for x in TEST_TG_CHAT_ID])
+TEST_TG_CHAT_ID = env_var('TEST_TG_CHAT_ID', '').split(',')
+TEST_TG_CHAT_ID.extend([int(x) for x in TEST_TG_CHAT_ID if x])
 
 FEEDBACK_TG_BOT = '@foodsharingsupportbot'
