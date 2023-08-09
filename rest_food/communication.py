a3aca4bfac48cc0a372db36f4126d5bbb0b52ab1
Dzmitry Dziamidaŭ <demidov91@users.noreply.github.com>
2023-08-10 00:09:55 +0200

    Localize cross-user messages correctly. (#24)

--- rest_food/communication.py
+++ rest_food/communication.py
@@ -21,31 +21,36 @@ from rest_food.states.supply_reply import (
 )
 from rest_food.states.formatters import build_demand_side_full_message_text_by_id
 from rest_food.translation import translate_lazy as _
-
+from user_utilities import user_language
 
 logger = logging.getLogger(__name__)
 
 
 def publish_supply_event(supply_user: User):
-    message = build_demand_side_short_message(supply_user, supply_user.editing_message_id)
     users = get_demand_users()
     random.shuffle(users)
-    get_mass_queue().push_super_batch(
-        message_and_chat_id=[(message, x.chat_id) for x in users],
-        workflow=Workflow.DEMAND
-    )
+
+    message_and_user_list = []
+
+    for user in users:
+        with user_language(user):
+            message = build_demand_side_short_message(supply_user, supply_user.editing_message_id)
+            message_and_user_list.append((message, user))
+
+    get_mass_queue().push_super_batch(message_and_user=message_and_user_list, workflow=Workflow.DEMAND)
 
 
 def notify_supply_for_booked(*, supply_user: User, message_id: str, demand_user: User):
-    reply = build_supply_side_booked_message(
-        demand_user=demand_user, supply_user=supply_user, message_id=message_id
-    )
+    with user_language(supply_user):
+        reply = build_supply_side_booked_message(
+            demand_user=demand_user, supply_user=supply_user, message_id=message_id
+        )
 
-    queue_messages(
-        tg_chat_id=int(supply_user.chat_id),
-        replies=[reply],
-        workflow=Workflow.SUPPLY,
-    )
+        queue_messages(
+            tg_chat_id=int(supply_user.chat_id),
+            replies=[reply],
+            workflow=Workflow.SUPPLY,
+        )
 
 
 def notify_demand_for_cancel(*, supply_user: User, message_id: str, message: str):
@@ -53,21 +58,22 @@ def notify_demand_for_cancel(*, supply_user: User, message_id: str, message: str
     if demand_user is None:
         raise ValueError('Demand user is not defined.')
 
-    food_description = build_demand_side_full_message_text_by_id(
-        supply_user=supply_user, message_id=message_id
-    )
-    text_to_send = _(
-        'Your request was rejected with the following words:\n%(message)s\n\nRequest was:\n%(food)s'
-    ) % {
-        'message': message,
-        'food': food_description,
-    }
+    with user_language(demand_user):
+        food_description = build_demand_side_full_message_text_by_id(
+            supply_user=supply_user, message_id=message_id
+        )
+        text_to_send = _(
+            'Your request was rejected with the following words:\n%(message)s\n\nRequest was:\n%(food)s'
+        ) % {
+            'message': message,
+            'food': food_description,
+        }
 
-    queue_messages(
-        tg_chat_id=int(demand_user.chat_id),
-        replies=[Reply(text=text_to_send)],
-        workflow=Workflow.DEMAND,
-    )
+        queue_messages(
+            tg_chat_id=int(demand_user.chat_id),
+            replies=[Reply(text=text_to_send)],
+            workflow=Workflow.DEMAND,
+        )
 
 
 def notify_demand_for_approved(*, supply_user: User, message_id: str):
@@ -75,15 +81,16 @@ def notify_demand_for_approved(*, supply_user: User, message_id: str):
     if demand_user is None:
         raise ValueError('Demand user is not defined.')
 
-    queue_messages(
-        tg_chat_id=int(demand_user.chat_id),
-        replies=[
-            build_demand_side_message_by_id(
-                supply_user, message_id, intro=_('Your request was approved')
-            )
-        ],
-        workflow=Workflow.DEMAND,
-    )
+    with user_language(demand_user):
+        queue_messages(
+            tg_chat_id=int(demand_user.chat_id),
+            replies=[
+                build_demand_side_message_by_id(
+                    supply_user, message_id, intro=_('Your request was approved')
+                )
+            ],
+            workflow=Workflow.DEMAND,
+        )
 
 
 def notify_admin_about_new_supply_user_if_necessary(supply_user: User):
@@ -92,7 +99,6 @@ def notify_admin_about_new_supply_user_if_necessary(supply_user: User):
         return
 
     admin_users = get_admin_users()
-    message = build_new_supplier_notification(supply_user)
     if not admin_users:
         logger.error("There are no admin users in db.")
         return 
@@ -104,44 +110,47 @@ def notify_admin_about_new_supply_user_if_necessary(supply_user: User):
                 admin_user.id
             )
         else:
-            queue_messages(
-                tg_chat_id=admin_user.chat_id,
-                replies=[message],
-                workflow=Workflow.SUPPLY
-            )
+            with user_language(admin_user):
+                queue_messages(
+                    tg_chat_id=admin_user.chat_id,
+                    replies=[build_new_supplier_notification(supply_user)],
+                    workflow=Workflow.SUPPLY
+                )
 
     set_info(supply_user, UserInfoField.IS_APPROVED_SUPPLY, None)
 
 
 def notify_supplier_is_approved(user: User):
-    queue_messages(
-        tg_chat_id=user.chat_id,
-        workflow=Workflow.SUPPLY,
-        replies=[
-            Reply(
-                text=_('Your account is approved!'),
-                buttons=[[{
-                    'data': f'c|{SupplyCommand.BACK_TO_POSTING}',
-                    'text': _('OK ✅'),
-                }]]
-            )
-        ],
-    )
+    with user_language(user):
+        queue_messages(
+            tg_chat_id=user.chat_id,
+            workflow=Workflow.SUPPLY,
+            replies=[
+                Reply(
+                    text=_('Your account is approved!'),
+                    buttons=[[{
+                        'data': f'c|{SupplyCommand.BACK_TO_POSTING}',
+                        'text': _('OK ✅'),
+                    }]]
+                )
+            ],
+        )
 
 
 def notify_supplier_is_declined(user: User):
-    queue_messages(
-        tg_chat_id=user.chat_id,
-        workflow=Workflow.SUPPLY,
-        replies=[
-            Reply(
-                text=(
-                    _('Your account was declined. Please, contact %s for any clarifications.') %
-                    FEEDBACK_TG_BOT
+    with user_language(user):
+        queue_messages(
+            tg_chat_id=user.chat_id,
+            workflow=Workflow.SUPPLY,
+            replies=[
+                Reply(
+                    text=(
+                        _('Your account was declined. Please, contact %s for any clarifications.') %
+                        FEEDBACK_TG_BOT
+                    )
                 )
-            )
-        ],
-    )
+            ],
+        )
 
 
 def queue_messages(
