d514b91725bd5abb928e277c159d7bc15090eed9
Dzmitry <demidov91@mail.ru>
2023-08-28 11:57:06 +0200

    Add Polish

--- compilemessages.sh
+++ compilemessages.sh
@@ -2,3 +2,4 @@ msgfmt rest_food/locale/ru/LC_MESSAGES/messages.po -o rest_food/locale/ru/LC_MES
 msgfmt rest_food/locale/be/LC_MESSAGES/messages.po -o rest_food/locale/be/LC_MESSAGES/messages.mo
 msgfmt rest_food/locale/en/LC_MESSAGES/messages.po -o rest_food/locale/en/LC_MESSAGES/messages.mo
 msgfmt rest_food/locale/uk/LC_MESSAGES/messages.po -o rest_food/locale/uk/LC_MESSAGES/messages.mo
+msgfmt rest_food/locale/pl/LC_MESSAGES/messages.po -o rest_food/locale/pl/LC_MESSAGES/messages.mo
--- createmessages.sh
+++ createmessages.sh
@@ -4,5 +4,6 @@ msgmerge rest_food/locale/ru/LC_MESSAGES/messages.po rest_food/locale/_common.po
 msgmerge rest_food/locale/be/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/be/LC_MESSAGES/messages.po --lang=be.UTF-8
 msgmerge rest_food/locale/en/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/en/LC_MESSAGES/messages.po --lang=be.UTF-8
 msgmerge rest_food/locale/uk/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/uk/LC_MESSAGES/messages.po --lang=be.UTF-8
+msgmerge rest_food/locale/pl/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/pl/LC_MESSAGES/messages.po --lang=be.UTF-8
 
 rm rest_food/locale/_common.po
