0e14ba91f67187da9fc712174671b1289229648e
Dzmitry <demidov91@mail.ru>
2023-08-31 10:55:01 +0200

    Update Belarusian localization.

--- createmessages.sh
+++ createmessages.sh
@@ -1,4 +1,4 @@
-find rest_food -iname "*.py" | xargs xgettext -L Python --from-code=utf-8 -o rest_food/locale/_common.po
+find rest_food -iname "*.py" | xargs xgettext -L Python --from-code=utf-8 --keyword=pgettext:1c,2 -o rest_food/locale/_common.po
 
 msgmerge rest_food/locale/ru/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/ru/LC_MESSAGES/messages.po --lang=ru.UTF-8
 msgmerge rest_food/locale/be/LC_MESSAGES/messages.po rest_food/locale/_common.po -o rest_food/locale/be/LC_MESSAGES/messages.po --lang=be.UTF-8
